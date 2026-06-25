d7970f96beda8936f901702eb2cafc5a8a8d865d
dothanhdat031009 <dothanhdat161200@gmail.com>
2026-06-26 00:31:23 +0700

    nothing

--- HelloWorld/Bai8.cpp
+++ HelloWorld/Bai8.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 #include <Windows.h>
-#include <string>
 
 using namespace std;
 
 int main()
 {
-    SetConsoleOutputCP(65001);
-    string ten = "Do Thanh Dat";
-    int tuoi = 0;
-    double chieuCao = 0;
-    cout << "Vui lòng nhập tên của bạn : ";
-    getline(cin,ten);
-    cout << "\n" << "Vui lòng nhập tuổi của bạn : ";
-    cin >> tuoi ;
-    cout << "\nVui lòng nhập chiều cao của bạn (đơn vị mét) : ";
-    cin >> chieuCao ;
-    cout << "\n" 
-         << "Tên bạn là : " << ten << endl
-         << "Bạn " << tuoi << " tuổi" << endl
-         << "Chiều cao bạn là : " << chieuCao << endl;
-    return 0;
+    int x , y ;
+    
+    cout << "Nhập x : ";
+    cin >> x ;
+    cout << "\n"
+    << "Nhập y : ";
+    cin >> y ;
+
+    double a = x ;
+    double b = y ;
+    double thuong = a / b ;
+
+    cout << "Thương của x / y là : " << thuong << endl;
+    return 0;    
 }
