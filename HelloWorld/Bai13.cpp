6c47e4e4f7364bd6d5b9c77c9f41c45517d12ff0
dothanhdat031009 <dothanhdat161200@gmail.com>
2026-06-27 00:15:01 +0700

    nothing

--- HelloWorld/Bai13.cpp
+++ HelloWorld/Bai13.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
 
-constexpr int cube(int a) { 
-    return a * a * a; 
-} 
+constexpr double dienTich(double x, double y)
+{
+    return x * x * 3.14159 + (y / 2) * (y / 2) * 3.14159 ;
+}
 
-int main() {
-    float gk[cube(3)]; 
-    
-    std::cout << "Array size is: " << sizeof(gk) / sizeof(float);
-    return 0;
+using namespace std ;
+
+int main()
+{
+    double x, y;
+    char doDai;
+    while (doDai != 'r' && doDai != 'd')
+    {
+        cout << "Bạn nhập bán kính hình tròn ( r ) hay đường kính hình tròn ( d ) : " ;
+        cin >> doDai ;
+        if(doDai != 'r' && doDai != 'd')
+        {
+            cout << "Vui lòng chọn đúng giá trị" << "\n";
+        }
+        if (doDai == 'r')
+        {
+            cout << "Nhập bán kính hình tròn : ";
+            cin >> x ;
+            y = 0;
+        }
+        if (doDai == 'd')
+        {
+            cout << "Nhập đường kính hình tròn : ";
+            cin >> y ;
+            x = 0;
+        }
+    }
+    cout << "\n"
+         << "Diện tích hình tròn là : " << dienTich(x, y) << endl;
+    return 0;    
 }
